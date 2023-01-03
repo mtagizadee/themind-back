e32a0922b77b415821dffcdf829ae413beb0f33c
mtaghizada <magatagi0@gmail.com>
2023-01-03 16:58:26 +0400

    .env files was added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
-  const PORT = 3000;
+  const PORT: number = parseInt(process.env.PORT) || 3000;
 
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(PORT, () => console.log(`SERVER STARTED ON ${PORT}`));
 }
