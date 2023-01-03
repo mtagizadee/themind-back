5f40ee14c2c0baa00ef207671c2c91b10860266b
mtaghizada <magatagi0@gmail.com>
2023-01-03 18:44:23 +0400

    add user function was added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,8 @@ import Redis from "ioredis";
 import { AddUserDto } from "./dto/add-user.dto";
 import { RedisKeys } from "src/common/enums";
 import { JwtService } from "@nestjs/jwt/dist";
+import { InternalServerErrorException } from "@nestjs/common/exceptions";
+import { AuthToken, Whitelist } from "./types";
 
 @Injectable()
 export class AuthService {
@@ -12,8 +14,23 @@ export class AuthService {
   /**
    * Adds user to the whitelist
    * @param addUserDto
+   * @returns AuthToken
+   * @throws InternalServerErrorException
    */
   async addUser(addUserDto: AddUserDto) {
     const { nickname } = addUserDto;
+    const jwtToken = this.jwt.sign({ nickname });
+    const whitelistJSON = await this.redis.get(RedisKeys.Whitelist);
+
+    try {
+      // add the token to the whitelist
+      const whitelist: Whitelist = JSON.parse(whitelistJSON);
+      whitelist.push(jwtToken);
+      await this.redis.set(RedisKeys.Whitelist, JSON.stringify(whitelist));
+
+      return { token: jwtToken } as AuthToken;
+    } catch (error) {
+      throw new InternalServerErrorException("Could not add user to the whitelist");
+    }
   }
 }
---
+++ src/auth/types/index.ts
@@ -0,0 +1,2 @@
+export type Whitelist = string[];
+export type AuthToken = { token: string };
