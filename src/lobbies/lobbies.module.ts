75037a3dacdab3357f4573537c1b8402bb6bf821
mtaghizada <magatagi0@gmail.com>
2023-01-04 20:07:04 +0400

    create lobby request is tested

--- src/lobbies/lobbies.module.ts
+++ src/lobbies/lobbies.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { LobbiesController } from './lobbies.controller';
-import { LobbiesService } from './lobbies.service';
+import { Module } from "@nestjs/common";
+import { LobbiesController } from "./lobbies.controller";
+import { LobbiesService } from "./lobbies.service";
 
 @Module({
   controllers: [LobbiesController],
-  providers: [LobbiesService]
+  providers: [LobbiesService],
 })
 export class LobbiesModule {}
