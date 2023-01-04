9fbc1f06e2e1ac1fe7141fe0f5e432771cff6ac6
mtaghizada <magatagi0@gmail.com>
2023-01-04 20:03:13 +0400

    lobby is created with one player inside

--- src/lobbies/lobbies.service.ts
+++ src/lobbies/lobbies.service.ts
@@ -23,7 +23,7 @@ export class LobbiesService {
       const lobbies = JSON.parse(await this.redis.get("lobbies"));
       lobbies[id] = {
         playersNumber,
-        players: [],
+        players: [{ id: userId }],
         authorId: userId,
       };
       await this.redis.set("lobbies", JSON.stringify(lobbies));
