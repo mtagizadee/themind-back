d9deb91836c1ab4b2340218489608e826421d2f1
mtaghizada <magatagi0@gmail.com>
2023-01-04 20:01:53 +0400

    end point for creating the lobby was added

--- src/lobbies/lobbies.controller.ts
+++ src/lobbies/lobbies.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, UseGuards, Body, Post } from "@nestjs/common";
+import { User } from "src/auth/decorators/user.decorator";
 import { JwtGuard } from "src/auth/guards/jwt.guard";
 import { CreateLobbyDto } from "./dto/create-lobby.dto";
+import { LobbiesService } from "./lobbies.service";
 
 @UseGuards(JwtGuard)
 @Controller("lobbies")
 export class LobbiesController {
+  constructor(private readonly lobbiesService: LobbiesService) {}
+
   @Post()
-  create(@Body() createLobbyDto: CreateLobbyDto) {
-    return "This action adds a new lobby";
+  create(@Body() createLobbyDto: CreateLobbyDto, @User("id") userId: string) {
+    return this.lobbiesService.create(createLobbyDto, userId);
   }
 }
--- src/lobbies/lobbies.service.ts
+++ src/lobbies/lobbies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
 import { InjectRedis } from "@liaoliaots/nestjs-redis";
 import Redis from "ioredis";
 import { CreateLobbyDto } from "./dto/create-lobby.dto";
@@ -8,7 +8,29 @@ import { v4 } from "uuid";
 export class LobbiesService {
   constructor(@InjectRedis() private readonly redis: Redis) {}
 
-  async create(createLobbyDto: CreateLobbyDto) {
+  /**
+   * Creates a new lobby
+   * @param createLobbyDto
+   * @returns the id of the newly created lobby
+   * @throws ConflictException if the lobby could not be created
+   */
+  async create(createLobbyDto: CreateLobbyDto, userId: string) {
+    const { playersNumber } = createLobbyDto;
     const id = v4();
+
+    try {
+      // Get the lobbies from the redis and add the new one
+      const lobbies = JSON.parse(await this.redis.get("lobbies"));
+      lobbies[id] = {
+        playersNumber,
+        players: [],
+        authorId: userId,
+      };
+      await this.redis.set("lobbies", JSON.stringify(lobbies));
+
+      return { id };
+    } catch (error) {
+      throw new ConflictException("Could not create a new lobby");
+    }
   }
 }
