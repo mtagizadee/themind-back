29972bc5311aace3e4bc7ba0fefba9cbcc8ba199
mtaghizada <magatagi0@gmail.com>
2023-01-28 14:46:58 +0400

    join and leave endpoints were removed from the lobbies controller

--- src/lobbies/lobbies.controller.ts
+++ src/lobbies/lobbies.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, UseGuards, Body, Post, Get, Param, Patch } from "@nestjs/common";
+import { Controller, UseGuards, Body, Post, Get, Param } from "@nestjs/common";
 import { User } from "src/auth/decorators/user.decorator";
 import { JwtGuard } from "src/auth/guards/jwt.guard";
-import { TJwtPayload } from "src/auth/strategy/jwt.strategy";
 import { CreateLobbyDto } from "./dto/create-lobby.dto";
 import { LobbiesService } from "./lobbies.service";
 import { lobbyResponseFactory } from "./types/lobby.type";
@@ -24,15 +23,5 @@ export class LobbiesController {
   @Post(":id/invitation-link")
   generateInvitationLink(@Param("id") id: string, @User("id") userId: string) {
     return this.lobbiesService.generateInvitationLink(id, userId);
-  }
-
-  @Patch(":id/join")
-  join(@Param("id") id: string, @User() user: TJwtPayload) {
-    return this.lobbiesService.join(id, user);
-  }
-
-  @Patch(":id/leave")
-  leave(@Param("id") id: string, @User("id") userId: string) {
-    return this.lobbiesService.leave(id, userId);
   }
 }
