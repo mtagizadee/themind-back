40fe6306ec018bb8bc7295aef9125b9aefae9882
mtaghizada <magatagi0@gmail.com>
2023-02-01 22:57:04 +0400

    Games controller was privated by JwtGuard

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get, Param } from "@nestjs/common";
+import { UseGuards } from "@nestjs/common/decorators";
+import { JwtGuard } from "src/auth/guards/jwt.guard";
 import { GamesService } from "./games.service";
 
+@UseGuards(JwtGuard)
 @Controller("games")
 export class GamesController {
   constructor(private readonly gamesService: GamesService) {}
